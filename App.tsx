fb5465af720c72d06799d33e9d991ea456187e38
maxpatrol <maxim.yarullin@gmail.com>
2023-01-20 22:16:18 +0300

    second

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const test = () => {
+  return (1)
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on!</Text>
+      <Text>{test()}</Text>
+      <Text>1</Text>
       <StatusBar style="auto" />
     </View>
   );
